cc698a52fb53df4d4ee91d5af5d1d117fbb34141
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 21:34:38 +0100

    feat(istio): install istio/gateway

--- 02_install-istio.sh
+++ 02_install-istio.sh
@@ -4,6 +4,10 @@
 helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster1
 helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster2
 
-# install istio/istiod
+## install istio/istiod
 helm install istiod istio/istiod -f ./cluster1/cluster.yaml -n istio-system --wait --kube-context=kind-cluster1
 helm install istiod istio/istiod -f ./cluster2/cluster.yaml -n istio-system --wait --kube-context=kind-cluster2
+
+## install istio/gateway
+helm install istio-ingressgateway istio/gateway -f ./cluster1/ew-gateway.yaml -n istio-system --wait --kube-context=kind-cluster1
+helm install istio-ingressgateway istio/gateway -f ./cluster2/ew-gateway.yaml -n istio-system --wait --kube-context=kind-cluster2
