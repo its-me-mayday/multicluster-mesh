ae2855835614ba12ed9d4f967e85d8c2e504899d
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 19:00:24 +0100

    style(cluster): move in a specific folder cluster and setup correct file in 01_configure-metallb.sh

--- 01_configure-metallb.sh
+++ 01_configure-metallb.sh
@@ -4,5 +4,5 @@
 docker network inspect -f '{{.IPAM.Config}}' kind
 
 # apply metallb in cluster1 and cluster2
-kubectl apply -f metallb.cluster1.yaml --context=kind-cluster1
-kubectl apply -f metallb.cluster2.yaml --context=kind-cluster2
+kubectl apply -f cluster1/metallb.yaml --context=kind-cluster1
+kubectl apply -f cluster2/metallb.yaml --context=kind-cluster2
