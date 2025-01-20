2a20e6accb1ff2780d37a4bce4c9a2759e0e7bf3
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 22:51:52 +0100

    style(*.sh): reorder files

--- 01_configure-metallb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# inspect network
-docker network inspect -f '{{.IPAM.Config}}' kind
-
-# apply metallb in cluster1 and cluster2
-kubectl apply -f cluster1/metallb.yaml --context=kind-cluster1
-kubectl apply -f cluster2/metallb.yaml --context=kind-cluster2
--- 01_install-n-configure-metallb.sh
+++ 01_install-n-configure-metallb.sh
@@ -3,3 +3,11 @@
 # install metallb (kubernetes bare metal cluster) by manifest
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml --context=kind-cluster1
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.14.9/config/manifests/metallb-native.yaml --context=kind-cluster2
+
+# inspect network
+docker network inspect -f '{{.IPAM.Config}}' kind
+
+# apply metallb in cluster1 and cluster2
+kubectl apply -f cluster1/metallb.yaml --context=kind-cluster1
+kubectl apply -f cluster2/metallb.yaml --context=kind-cluster2
+
