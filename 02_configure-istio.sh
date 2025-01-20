ab0b9a211204ff624a533de1ab2f41f89b7710e4
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 19:25:24 +0100

    feat(istio): create secrets

--- 02_configure-istio.sh
+++ 02_configure-istio.sh
@@ -3,3 +3,17 @@
 # apply namespace in cluster1 and cluster2
 kubectl apply -f cluster1/namespace.yaml --context=kind-cluster1
 kubectl apply -f cluster2/namespace.yaml --context=kind-cluster2
+
+# apply secrets (kind-cluster1)
+kubectl create secret generic cacerts -n istio-system \
+     --from-file=../istio-1.24.2/certs/cluster1/ca-cert.pem \
+     --from-file=../istio-1.24.2/certs/cluster1/ca-key.pem \
+     --from-file=../istio-1.24.2/certs/cluster1/root-cert.pem \
+     --from-file=../istio-1.24.2/certs/cluster1/cert-chain.pem --context=kind-cluster1
+
+# apply secrets (kind-cluster2)
+kubectl create secret generic cacerts -n istio-system \
+     --from-file=../istio-1.24.2/certs/cluster2/ca-cert.pem \
+     --from-file=../istio-1.24.2/certs/cluster2/ca-key.pem \
+     --from-file=../istio-1.24.2/certs/cluster2/root-cert.pem \
+     --from-file=../istio-1.24.2/certs/cluster2/cert-chain.pem --context=kind-cluster2
