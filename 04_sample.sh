2408440415358bb1a599c533f3c6313359c86c16
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 22:08:42 +0100

    feat(sample): setup helloworld app

---
+++ 04_sample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# create namespace
+kubectl create namespace istio-sample --context=kind-cluster1
+kubectl create namespace istio-sample --context=kind-cluster2
+
+# setup label istio-injection
+kubectl label ns istio-sample istio-injection=enabled --context=kind-cluster1
+kubectl label ns istio-sample istio-injection=enabled --context=kind-cluster2
+
+# install helloworld (services) in both clusters
+kubectl apply -f ./samples/helloworld.yaml -l service=helloworld -n istio-sample --context=kind-cluster1
+kubectl apply -f ./samples/helloworld.yaml -l service=helloworld -n istio-sample --context=kind-cluster2
+
+# install helloworld (deployments) in both clusters
+kubectl apply -f ./samples/helloworld.yaml -l version=v1 -n istio-sample --context=kind-cluster1
+kubectl apply -f ./samples/helloworld.yaml -l version=v2 -n istio-sample --context=kind-cluster2
+
+kubectl apply -f ./samples/sleep.yaml -n istio-sample --context=kind-cluster1
+kubectl apply -f ./samples/sleep.yaml -n istio-sample --context=kind-cluster2
