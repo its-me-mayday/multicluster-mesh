87cf74e5ef6ec60e94dc328c1a9435262a64ae0f
Luca Maggio <lucamaggio1992@gmail.com>
2025-01-20 21:26:00 +0100

    feat(istio): uncomment helm install istio/base

--- 02_install-istio.sh
+++ 02_install-istio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install istio/base
-#helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster1
-#helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster2
+helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster1
+helm install istio-base istio/base -n istio-system --wait --kube-context=kind-cluster2
 
 # install istio/istiod
 helm install istiod istio/istiod -f ./cluster1/cluster.yaml -n istio-system --wait --kube-context=kind-cluster1
